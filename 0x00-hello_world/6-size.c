4982bd6ccf3f1fc4f06267f1e61dff23f328d742
Chidinma <ekechydma@gmail.com>
2022-06-07 06:18:22 -0700

    jjjsttg

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,17 +8,17 @@
 
 int main(void)
 {
-	float adecimal
-	char astring
-	long alongstr
-	long long alongerstr
-	int awholenum
+	float adecima;
+	char astring;
+	long alongstr;
+	long long alongerstr;
+	int awholenum;
 
 	printf("Size of a char: %lu byte(s)\n", sizeof(astring));
 	printf("Size of an int: %lu byte(s)\n", sizeof(awholenum));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(alongstr));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(alongerstr));
-	printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
+	printf("Size of a float: %lu byte(s)\n", sizeof(adecima));
 
 	return (0);
-}
+} 
