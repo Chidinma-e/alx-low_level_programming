42a3028fd3d5ec5cba20c5670fd075687fdcb7a3
Chidinma <ekechydma@gmail.com>
2022-06-07 05:10:22 -0700

    corrected again

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 /**
- * Create a function that writes ' @Programming is like building a multilingual puzzle ' 
- * Return: Always 0 (shows code is successful)
+ * main - Write 'Programming is like building a multilingual puzzle'
+ * Return - 0 (Success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
-}
+	puts("\"Programming is like a multilingual puzzle");
+	return (0);
+} 
