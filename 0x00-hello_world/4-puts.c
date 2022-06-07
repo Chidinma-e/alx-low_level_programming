146138145f9cdae06ad96a36fea9fbb5b03f2724
Chidinma <ekechydma@gmail.com>
2022-06-07 04:57:42 -0700

    httt

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * Create a function that writes ' @Programming is like building a multilingual puzzle ' 
+ * Return: Always 0 (shows code is successful)
+ */
+int main(void)
+{
+puts("/"Programming is like building a multilingual puzzle");
+return (0);
+}
